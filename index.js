07aafd0cb5693582ce6010bfb541f1f748bac59d
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2023-10-21 03:35:51 -0500

    need to add initial render state

---
+++ index.js
@@ -0,0 +1,29 @@
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+
+const randomFreelancerArray = [];
+
+function (){
+
+}
+
+function addFreelancer() {
+  const randomFreelancer =
+    freelancers[Math.floor(Math.random() * freelancers.length)];
+  if (!randomFreelancerArray.includes(randomFreelancer)) {
+    randomFreelancerArray.push(randomFreelancer);
+    const table = document.querySelector(".freelancer-info");
+    const newRow = table.insertRow(-1);
+    newRow.innerHTML = `<td>${randomFreelancer.name}</td><td>${randomFreelancer.occupation}</td><td>${randomFreelancer.price}</td>`;
+  }
+}
+
+setInterval(addFreelancer, 3000);
